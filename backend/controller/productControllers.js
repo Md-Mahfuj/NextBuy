01f152060498a6e11b3175cd3463719fe96fa782
Md-Mahfuj <mdraju0162735@gmail.com>
2023-06-27 12:46:28 +0600

    Backend Search, Filters & Pagination

--- backend/controller/productControllers.js
+++ backend/controller/productControllers.js
@@ -1,5 +1,7 @@
 import Product from "../model/product";
 
+import APIFilters from "../utils/APIFilters";
+
 export const newProduct = async (req,res,next)=>{
     const product = await Product.create(req.body);
 
@@ -8,12 +10,41 @@ export const newProduct = async (req,res,next)=>{
     });
 }
 
-export const getProducts = async (req,res,next)=>{
-    const products = await Product.find();
+// export const getProducts = async (req,res,next)=>{
+
+//     const apiFilters = new APIFilters(Product.find(), req.query).search().filter()
+//     const products = await apiFilters.query ;
+//     console.log("products",products);
+//     res.status(200).json({
+//         products,
+//     });
+// }
+
+export const getProducts = async (req, res, next) => {
+    const resPerPage = 3;
+    const productsCount = await Product.countDocuments();
+  
+    const apiFilters = new APIFilters(Product.find(), req.query)
+      .search()
+      .filter();
+  
+    let products = await apiFilters.query;
+    const filteredProductsCount = products.length;
+  
+    apiFilters.pagination(resPerPage);
+  
+    products = await apiFilters.query.clone();
+  
     res.status(200).json({
-        products,
+      productsCount,
+      resPerPage,
+      filteredProductsCount,
+      products,
     });
-}
+  };
+  
+
+
 
 export const getProduct = async (req,res,next)=>{
     const product = await Product.findById(req.query.id);
