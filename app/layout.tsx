8a1d70865b434cab8102064997f452c17cca586c
Md-Mahfuj <mdraju0162735@gmail.com>
2023-07-05 23:30:30 +0600

    add cart function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Header from '../components/layouts/Header';
+import  GlobalProvider  from "./GlobalProvider";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <GlobalProvider> 
         <Header />
         {children}
+        </GlobalProvider>
         </body>
     </html>
   )
