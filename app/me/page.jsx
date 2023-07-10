d3675a7c18fdf84a86bb08b179e8072cdc7c7301
Md-Mahfuj <mdraju0162735@gmail.com>
2023-07-10 11:45:09 +0600

    add user address

--- app/me/page.jsx
+++ app/me/page.jsx
@@ -1,8 +1,17 @@
 import Profile from "../../components/auth/Profile";
 import React from "react";
+import axios from "axios";
+
+const getAddresses = async () => {
+  const { data } = await axios.get(`${process.env.API_URL}/api/address`);
+
+  return data?.addresses;
+};
 
 const ProfilePage = async () => {
-  return <Profile />;
+  const addresses = await getAddresses();
+
+  return <Profile addresses={addresses} />;
 };
 
 export default ProfilePage;
